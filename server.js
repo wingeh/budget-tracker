e491c1abe02c6c88a896edb9bb4f71c5069bb74f
Andrew <wing_andrew@hotmail.com>
2021-08-13 22:50:19 -0400

    Undoing previous commit and checking server routes

--- server.js
+++ server.js
@@ -16,20 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
-const db = mongoose.connection;
-
-db.on('error', (error) => console.error(error));
-db.once('open', () => {
-  console.log('Connected To Database');
-  app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  })
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
